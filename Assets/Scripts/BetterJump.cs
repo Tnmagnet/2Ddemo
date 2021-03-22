66ec5d079a64ee1e06c971c7c1d6f8b3be314238
Tn_magnet <40493125+zhaojunyip@users.noreply.github.com>
2021-03-22 16:10:50 +0800

    wallJump.dash finish

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BetterJump : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+
+    private float fallMultiplier = 2.5f;
+
+    private float lowJumpMultiplier = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_rigidbody2D.velocity.y < 0)
+        {
+            _rigidbody2D.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (_rigidbody2D.velocity.y > 0)
+        {
+            _rigidbody2D.velocity += Vector2.up
+                                     * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Movement : MonoBehaviour
 {
@@ -32,11 +35,10 @@ public class Movement : MonoBehaviour
     private bool dash;
     private bool jump;
     private bool wallJump;
-
-    [Space]
-
+    
     private bool groundTouch;
     private bool hasDashed;
+    private bool hasJumped;
 
     public int side = 1;
     // Start is called before the first frame update
@@ -59,7 +61,7 @@ public class Movement : MonoBehaviour
         if (_collision.onGround && !isDashing)
         {
             hasDashed = false;
-            //TODO:开启更自然的跳跃
+            GetComponent<BetterJump>().enabled = true;
         }
         //抓墙成功
         if (Input.GetButton("Fire3") && _collision.onWall && canMove)
@@ -87,14 +89,14 @@ public class Movement : MonoBehaviour
             wallSlide = false;
         }
 
-        if (Input.GetButton("Fire1") && !hasDashed)
+        if (Input.GetButton("Fire1") && !hasDashed&&(xRaw!=0||yRaw!=0))
         {
             dash = true;
         }
         //墙边跳及普通跳跃
         if (Input.GetButton("Jump"))
         {
-            if (_collision.onGround)
+            if (_collision.onGround&&!hasJumped)
             {
                 jump = true;
             }
@@ -104,11 +106,31 @@ public class Movement : MonoBehaviour
                 wallJump = true;
             }
         }
+        //判断墙的方向
+        if (x > 0)
+        {
+            side = 1;
+            _rigidbody2D.transform.eulerAngles=new Vector3(0f,0f,0f);
+        }
+
+        if (x < 0)
+        {
+            side = -1;
+            _rigidbody2D.transform.eulerAngles=new Vector3(0f,180f,0f);
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        if (jump)
+        {
+            Jump(Vector2.up);
+        }
+
+        if (dash)
+        {
+            Dash(xRaw,yRaw);
+        }
     }
 
     void Run(Vector2 direction)
@@ -117,6 +139,7 @@ public class Movement : MonoBehaviour
             return;
         if(wallGrab)
             return;
+        _animator.SetBool("run",true);
         if (!wallJumped)
         {
             _rigidbody2D.velocity=new Vector2(direction.x*speed,_rigidbody2D.velocity.y);
@@ -128,25 +151,49 @@ public class Movement : MonoBehaviour
         }
     }
 
-    void Jump(Vector2 direction, bool onWall)
+    void Jump(Vector2 direction)
     {
+        _animator.SetBool("jump",true);
         var velocity = _rigidbody2D.velocity;
         velocity=new Vector2(velocity.x,0);//y方向速度清零
         velocity += direction * jumpForce;
         _rigidbody2D.velocity = velocity;
     }
 
-    void Dash(Vector2 direction)
+    void Dash(float x,float y)
     {
         _rigidbody2D.velocity = Vector2.zero;
-        _rigidbody2D.velocity += direction * dashSpeed;
+        _rigidbody2D.velocity += new Vector2(x,y) * dashSpeed;
         StartCoroutine(DashWait());
     }
 
     IEnumerator DashWait()
     {
         _rigidbody2D.gravityScale = 0;
+        GetComponent<BetterJump>().enabled = false;
         yield return new WaitForSeconds(0.3f);
         _rigidbody2D.gravityScale = 3;
+        GetComponent<BetterJump>().enabled = true;
+    }
+
+    void WallJump()
+    {
+        if ((side == 1 && _collision.onRightWall) || (side == -1 && _collision.onLeftWall))
+        {
+            side *= -1;
+            _rigidbody2D.transform.eulerAngles=new Vector3(0f,180f,0f);
+        }
+        StopCoroutine(DisableMovement(0));
+        StartCoroutine(DisableMovement(0.1f));
+        Vector2 wallDir = _collision.onRightWall ? Vector2.left : Vector2.right;
+        Jump(Vector2.up/1.5f+wallDir/1.5f);
+        wallJumped = true;
+    }
+
+    IEnumerator DisableMovement(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
     }
 }
